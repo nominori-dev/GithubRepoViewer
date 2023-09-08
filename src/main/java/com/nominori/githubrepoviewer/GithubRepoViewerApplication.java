b784bf03bee32b1179b91f59f295b99ca123a6b5
nominori-dev <antonshimchico37@gmail.com>
2023-09-08 15:27:36 +0200

    feat: project initialization

---
+++ src/main/java/com/nominori/githubrepoviewer/GithubRepoViewerApplication.java
@@ -0,0 +1,13 @@
+package com.nominori.githubrepoviewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubRepoViewerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubRepoViewerApplication.class, args);
+    }
+
+}
